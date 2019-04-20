2df20ae1f8639e986a9fe80f8fee477b21f804c5
yuliya.ditarova <yuliya.ditarova@rtlabs.ru>
2019-04-20 23:44:43 +0300

    Patient service

--- patients-service/src/main/java/ru/rtlabs/hackaton/service/PatientServiceImpl.java
+++ patients-service/src/main/java/ru/rtlabs/hackaton/service/PatientServiceImpl.java
@@ -1,4 +1,52 @@
 package ru.rtlabs.hackaton.service;
 
+import org.hl7.fhir.dstu3.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.rtlabs.hackaton.repository.PatientRepository;
+
+import java.util.Optional;
+
+@Service
 public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+
+    @Override
+    public Iterable<?> findAll() {
+        return patientRepository.findAll();
+    }
+
+    @Override
+    public Patient findByIdMyUnqualifiedId(String id) {
+        Optional<Patient> optionalPatient = patientRepository.findByIdMyUnqualifiedId(id);
+        return optionalPatient.orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public Patient save(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public Patient update(String id, Patient patient) {
+        Optional<Patient> optionalPatient = patientRepository.findByIdMyUnqualifiedId(id);
+        Patient update = optionalPatient.orElseThrow(IllegalArgumentException::new);
+        update.setId(patient.getId());
+        update.setName(patient.getName());
+        update.setGender(patient.getGender());
+        update.setBirthDate(patient.getBirthDate());
+        return patientRepository.save(update);
+    }
+
+    @Override
+    public void deleteByIdMyUnqualifiedId(String id) {
+        patientRepository.deleteByIdMyUnqualifiedId(id);
+    }
 }
