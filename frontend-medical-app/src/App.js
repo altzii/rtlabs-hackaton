ce1b2f07bdf53f9ba9a4413bbd69fe6ec0569b40
Alexander Leontyev <sleontjev@yandex.ru>
2019-04-20 20:50:44 +0300

    Add table example

--- frontend-medical-app/src/App.js
+++ frontend-medical-app/src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Table} from 'reactstrap';
+import {Button} from 'reactstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container">
+                <Table>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>BirthDate</th>
+                        <th>Name</th>
+                        <th>Gender</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>09.09.2009</td>
+                        <td>Almaz</td>
+                        <td>FEMALE</td>
+                        <td>
+                            <Button color="success" size="sm" className="mr-2">Edit</Button>
+                            <Button color="danger" size="sm">Delete</Button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>04.04.1996</td>
+                        <td>Aleksandr</td>
+                        <td>MALE</td>
+                        <td>
+                            <Button color="success" size="sm" className="mr-2">Edit</Button>
+                            <Button color="danger" size="sm">Delete</Button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>01.01.2020</td>
+                        <td>Yulia</td>
+                        <td>FEMALE</td>
+                        <td>
+                            <Button color="success" size="sm" className="mr-2">Edit</Button>
+                            <Button color="danger" size="sm">Delete</Button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
 
 export default App;
